f9d40ad279483e3155cfb484928e3beeab2bf49b
Gianluca Starke <gianlucastarke@gmail.com>
2022-07-21 16:18:21 -0300

    Usability alterations
    Create src folder
    
    included src/enviarEmail.py
    
    main.py now is more readable

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-import smtplib, ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from src.enviarEmail import enviarEmail
 
 def main():
-    # Server info
-    smtp_server = 'mail.sampaio-sa.com.br'
-    port = 465
 
-    # User info
     user_mail = input('EMAIL: ')
     password = input('SENHA: ')
+    receiver_mail = input('DESTINATÁRIO: ')
+    html = '''
+        <html>
+            <body>
+                <p>
+                    example plain text.
+                    <br><br>
+                    example plain text.
+                </p>
+            <body>
+        </html>
+        '''
 
-    # Mail info
-    mail = MIMEMultipart('alternative')
-    mail['Subject'] = 'Teste'
-    mail['From'] = user_mail
-    mail['To'] = input('DESTINATÁRIO: ')
-    mail.attach(
-        MIMEText('''\
-            <html>
-                <body>
-                    <p>
-                        example plain text.
-                        <br><br>
-                        example plain text.
-                    </p>
-                <body>
-            </html>
-            ''',
-        'html')
-    )
-
-    # Context
-    context = ssl.create_default_context()
-
-    # Create server and send email
-    with smtplib.SMTP_SSL(smtp_server, port, context = context) as server:
-        server.login(user_mail, password)
-        server.sendmail(user_mail, mail['To'], mail.as_string())
+    enviarEmail(user_mail, password, receiver_mail, html)
 
 
 if __name__ == '__main__':
