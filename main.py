f76928a25e7918dd9607d6e0e1bfdb55164c920a
Gianluca Starke <gianlucastarke@gmail.com>
2022-07-21 14:31:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def main():
     port = 465
 
     # User info
-    user_mail = 'gianluca.ar@sampaio-sa.com.br'
-    password = ''
+    user_mail = input('digite o email: ')
+    password = input('digite a senha: ')
 
-    # Mail text
-    text = 'example plain text'
+    # Mail info
+    receiver_mail = 'gianluca.ar@sampaio-sa.com.br'
+    text = '''\
+    example plain text.
+    
+    example plain text.'''
 
     # Context
     context = ssl.create_default_context()
@@ -20,8 +24,7 @@ def main():
     # Create server and send email
     with smtplib.SMTP_SSL(smtp_server, port, context = context) as server:
         server.login(user_mail, password)
-        server.sendmail(user_mail, user_mail, text)
-        server.quit()
+        server.sendmail(user_mail, receiver_mail, text)
 
 if __name__ == '__main__':
     try:
