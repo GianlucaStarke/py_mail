08038b1425e25c472281bf31354d03ec5aa50808
Gianluca Starke <gianlucastarke@gmail.com>
2022-07-21 17:15:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from src.enviarEmail import enviarEmail
 
 def main():
 
-    user_mail = input('EMAIL: ') # seu email
-    password = input('SENHA: ') # sua senha de login
-    receiver_mail = input('DESTINATÁRIO: ') # email do destinatario
-    html = '''
-        <html>
-            <body>
-                <p>
-                    example plain text.
-                    <br><br>
-                    example plain text.
-                </p>
-            <body>
-        </html>
-        '''
+    # user_mail = input('EMAIL: ') # seu email
+    # password = input('SENHA: ') # sua senha de login
+    # receiver_mail = input('DESTINATÁRIO: ') # email do destinatario
+    # html = '''
+    #     <html>
+    #         <body>
+    #             <p>
+    #                 example plain text.
+    #                 <br><br>
+    #                 example plain text.
+    #             </p>
+    #         <body>
+    #     </html>
+    #     '''
 
-    enviarEmail(user_mail, password, receiver_mail, html)
+    # enviarEmail(user_mail, password, receiver_mail, html)
+
+    df = pd.readExcel('planilha_ajuste_apuracao.xlsx')
+
+    print(df)
 
 
 if __name__ == '__main__':
